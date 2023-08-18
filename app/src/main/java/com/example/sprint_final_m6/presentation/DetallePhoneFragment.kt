8a0233f277625bab24d2649896c0f822ea274460
javo2023 <jguzmanalarcon@gmail.com>
2023-08-17 22:10:14 -0400

    Se implementa scroll

--- app/src/main/java/com/example/sprint_final_m6/presentation/DetallePhoneFragment.kt
+++ app/src/main/java/com/example/sprint_final_m6/presentation/DetallePhoneFragment.kt
@@ -12,14 +12,8 @@ import coil.load
 import com.example.sprint_final_m6.R
 import com.example.sprint_final_m6.databinding.FragmentDetallePhoneBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private var ARG_PARAM1 = "id"
-/*private const val ARG_PARAM2 = "name"
-private const val ARG_PARAM3 = "price"
-private const val ARG_PARAM4 = "image"
 
- */
+private var ARG_PARAM1 = "id"
 
 
 class DetallePhoneFragment : Fragment() {
@@ -28,24 +22,17 @@ class DetallePhoneFragment : Fragment() {
 
     private val phoneVM: PhoneVM by activityViewModels()
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    /*private var param2: String? = null
-    private var param3: String? = null
-    private var param4: String? = null
 
-     */
+    private var param1: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
-            /*param2 = it.getString(ARG_PARAM2)
-            param3 = it.getString(ARG_PARAM3)
-            param4 = it.getString(ARG_PARAM4)
 
-             */
         }
     }
 
@@ -53,15 +40,35 @@ class DetallePhoneFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = FragmentDetallePhoneBinding.inflate(layoutInflater, container, false)
         phoneVM.getDetailPhoneVM(param1.toString())
         getDetailPhone()
 
+        binding.btnMessage.setOnClickListener(View.OnClickListener {
+            sendEmail("Nokia","id")
+        })
+
         return binding.root
 
-        binding.btnMessage.setOnClickListener(View.OnClickListener {
-            sendEmail("Nokia","1")
-        })
+
+    }
+
+    private fun sendEmail(name: String, id: String) {
+        val emailSubject = "Consulta $name id $id"
+        val emailMessage =
+            "Hola,\n\nVi el teléfono $name de código $id y me gustaría que me contactaran a este correo o al siguiente número.\n\nQuedo atento."
+
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:info@novaera.cl")
+        intent.putExtra(Intent.EXTRA_SUBJECT, emailSubject)
+        intent.putExtra(Intent.EXTRA_TEXT, emailMessage)
+
+
+
+
+        startActivity(intent)
+
 
 
     }
@@ -86,6 +93,8 @@ class DetallePhoneFragment : Fragment() {
 
 
                 }
+
+
             }
 
         }
@@ -93,21 +102,7 @@ class DetallePhoneFragment : Fragment() {
 
 
     }
-    fun sendEmail(name: String, id: String) {
-        val emailSubject = "Consulta $name id $id"
-        val emailMessage =
-            "Hola,\n\nVi el teléfono $name de código $id y me gustaría que me contactaran a este correo o al siguiente número.\n\nQuedo atento."
 
-        val intent = Intent(Intent.ACTION_SENDTO)
-        intent.data = Uri.parse("mailto:jguzmanalarcon@gmail.com")
-        intent.putExtra(Intent.EXTRA_SUBJECT, emailSubject)
-        intent.putExtra(Intent.EXTRA_TEXT, emailMessage)
-
-
-        startActivity(intent)
-
-
-    }
 
 
 }
